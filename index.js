b76b69486a03475c769d954efae7081b118da9f2
Arno van den Berg <arvandenberg@gmail.com>
2018-07-07 12:31:26 +0200

    move from gulp to express

---
+++ index.js
@@ -0,0 +1,30 @@
+/// START
+var express = require('express');
+var favicon = require('serve-favicon')
+var path = require('path')
+var app = express();        
+app.use(favicon(path.join(__dirname,'public','img','favicon.ico')));
+
+var bodyParser= require('body-parser')
+var sslRedirect = require('heroku-ssl-redirect');
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+// enable ssl redirect
+app.use(sslRedirect());
+
+app.use('/public', express.static(path.join(__dirname + "/public")));
+app.use('/vendor', express.static(path.join(__dirname + "/vendor")));
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/index.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
